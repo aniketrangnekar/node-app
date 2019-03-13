8b5f906c64b14d74bd74b6be6b68d0867bf9ec9f
Ubuntu <ubuntu@ip-172-31-37-236.ap-northeast-1.compute.internal>
2019-03-13 10:26:10 +0000

    hello awesome changed to hello there

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hello awesome', function(done) {
-  //navigate to root and check the response is "hello awesome"
-  request(app).get('/').expect('hello awesome', done);
+ it('respond with hello there', function(done) {
+  //navigate to root and check the response is "hello there"
+  request(app).get('/').expect('hello there', done);
  });
 });
