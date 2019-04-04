44093de65c43582018ee6a671d55f6e419f66893
Ubuntu <ubuntu@ip-172-31-37-236.ap-northeast-1.compute.internal>
2019-04-04 07:41:59 +0000

    changed prot no

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var express = require('express');
 var app = express();
 //Respond with "hello awesome" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello there');
+ res.send('hello awesome');
 });
-//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+//listen to port 4000 by default
+app.listen(process.env.PORT || 4000);
  
 module.exports = app;
 
